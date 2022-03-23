6152a56a0a07962e1f2bf4fce6046d73ac9bcfa2
Melanie Arellano <melaniesigrid@protonmail.com>
2022-03-23 11:51:03 -0600

    Complete app functionality

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
 
 function App() {
   const [user] = useAuthState(auth);
--- src/components/ChatRoom.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ChatRoom() {
-  return (
-    <div>ChatRoom</div>
-  );
-}
-
-export default ChatRoom;
